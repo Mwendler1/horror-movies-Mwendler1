e7d766adf87b9563142dd8aa8e659d44bb099190
Matthew Wendler <102497931+Mwendler1@users.noreply.github.com>
2023-10-29 23:06:28 -0400

    Update horror_movies.sql

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,6 @@
--- Add your SQL here
+-SELECT id AS 'Movie_ID', name AS 'Movie_Title', imbd_rating AS 'Ratings'
+FROM movies
+WHEREgenre = 'horror'
+    AND year <= 1985
+ORDER BY imbd_rating DESC
+LIMIT 3;
